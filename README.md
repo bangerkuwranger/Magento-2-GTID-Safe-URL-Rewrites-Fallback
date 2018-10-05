620696a7b0122314c4940f27f06a4cd485fec5c5
Chad A. Carino <ccarino@sundialbrands.com>
2018-10-05 17:17:56 -0400

    Final release version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Magento 2 GTID Safe URL Rewrite Fallback
 
-Magento 2 module that forces the category save observer to fall back to a method that doesn't require temporary tables. This allows GTID consistent category saves, at the expense of some performance.
+Magento 2 module that forces the category save/update observers to fall back to a method that doesn't require temporary tables. This allows GTID consistent category saves, at the expense of some performance.
 
 ## Why?
 
--- registration.php
+++ registration.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Copyright © 2017 Chad A. Carino. All rights reserved.
+ * Copyright © 2017-2018 Chad A. Carino. All rights reserved.
  * See LICENSE file for license details.
  *
  * @package    Bangerkuwranger/GtidSafeUrlRewriteFallback
  * @author     Chad A. Carino <artist@chadacarino.com>
  * @author     Burak Bingollu <burak.bingollu@gmail.com>
- * @copyright  2017 Chad A. Carino
+ * @copyright  2017-2018 Chad A. Carino
  * @license    https://opensource.org/licenses/MIT  MIT License
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
